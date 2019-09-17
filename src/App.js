c6df166ce52828bd6cf655f154099038423ca6da
Sione Tenifa <johntenifa@yahoo.com>
2019-09-17 12:51:24 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component, } from 'react';
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: 1, name: "Learn Rails", complete: true, },
+      { id: 2, name: "Learn React", complete: false, },
+      { id: 3, name: "Graduate DPL", complete: false, },
+    ]
+  };
+
+renderTodos = () => {
+  const { todos, } = this.state;
+  return todos.map( todo => 
+    <li key={todo.id}>{todo.name}</li>
+  )
+};
+
+  render() {
+    return (
+      <div>
+        <ul>
+          { this.renderTodos() }
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default App;
