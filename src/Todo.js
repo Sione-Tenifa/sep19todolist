f85429ba66a2445578412412b9d8c75b654ec63b
Sione Tenifa <johntenifa@yahoo.com>
2019-09-19 15:37:59 -0600

    finished small appp

--- src/Todo.js
+++ src/Todo.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-const Todo = ({ id, name, complete }) => (
+const Todo = ({ id, name, complete, todoClick }) => (
   <li
     style={ complete ? { ...styles.todo, ...styles.complete } : styles.todo }
+    onClick={ () => todoClick(id) }
   >
    { name }
   </li>
